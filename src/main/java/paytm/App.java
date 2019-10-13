b9386df4565600c17c7d576f28529338bb35bebd
Susrut Patnaik <susrutpatnaik@gmail.com>
2019-10-14 01:04:23 +0530

    java code for addmoney

---
+++ src/main/java/paytm/App.java
@@ -0,0 +1,9 @@
+package paytm;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/paytm/AppTest.java
@@ -0,0 +1,12 @@
+package paytm;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
